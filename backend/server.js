c1e91114cb53f352d4ca7b6654d5c74a5a824d41
3duu21 <e5470843@gmail.com>
2024-07-01 03:02:13 -0400

    arreglo proxy2

--- backend/server.js
+++ backend/server.js
@@ -11,12 +11,12 @@ app.use(express.json())
 app.use("/api/portfolio", portfolioRoute)
 const path = require("path")
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "client/build")))
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "client/build/index.html"))
-    })
-}
+// if (process.env.NODE_ENV === "production") {
+//     app.use(express.static(path.join(__dirname, "client/build")))
+//     app.get("*", (req, res) => {
+//         res.sendFile(path.join(__dirname, "client/build/index.html"))
+//     })
+// }
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`)
