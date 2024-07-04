9316ede8d0dd141ca602e4df9f6b35cfd15a1e66
3duu21 <e5470843@gmail.com>
2024-07-03 22:47:14 -0400

    cambios en db config

--- backend/config/dbConfig.js
+++ backend/config/dbConfig.js
@@ -4,10 +4,14 @@ const mongoose = require('mongoose');
 const mongoURI = process.env.mongo_url;
 
 mongoose.connect(mongoURI, {
-  useNewUrlParser: true,  // Puedes probar eliminar esto si sigue dando problemas
-  useUnifiedTopology: true,  // Puedes probar eliminar esto si sigue dando problemas
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
   ssl: true,
-  // tlsInsecure: true  // Desactiva la validación de certificados SSL (solo para pruebas)
+  tlsInsecure: true  // Desactiva la validación de certificados SSL (solo para pruebas)
+}).then(() => {
+  console.log('Connected to database with SSL but without strict validation');
+}).catch((err) => {
+  console.error('Error connecting to database:', err);
 });
 
 const connection = mongoose.connection;
