dd135dbcfdb90e7e9c08354bb89023381c65f178
3duu21 <e5470843@gmail.com>
2024-07-05 02:31:08 -0400

    agregamos cors al server

--- backend/server.js
+++ backend/server.js
@@ -1,43 +1,41 @@
 const express = require('express');
-const https = require('https');
-const fs = require('fs');
+const mongoose = require('mongoose');
+const cors = require('cors');
 const path = require('path');
 require('dotenv').config();
-// const dbConfig = require('./config/dbConfig');
-// import { fileURLToPath } from 'url';
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-// Rutas
-const portfolioRoute = require('./routes/portfolioRoute');
-const { default: mongoose } = require('mongoose');
-
 // Middleware
 app.use(express.json());
-app.use('https://portafolio-eduardo-66pp.onrender.com/api/portfolio', portfolioRoute);
+
+// Configurar CORS
+const corsOptions = {
+  origin: 'https://6687923d61200ef2e1114ff1--nimble-kitten-cff20c.netlify.app/', // Reemplaza con el dominio de tu aplicación React
+  optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
+
+// Rutas
+const portfolioRoute = require('./routes/portfolioRoute');
+app.use('/api/portfolio', portfolioRoute);
 
 // Configuración de producción
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/client/build/index.html'));
-})
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
 
-//Use the client app
-app.use(express.static(path.join(__dirname, '/client/build')))
+// Use the client app
+app.use(express.static(path.join(__dirname, '/client/build')));
 
- 
-// app.listen(port, () => {
-//     console.log(`Server listening on port ${port}`)
-// })
+// Conectar a la base de datos
+mongoose.connect(process.env.mongo_url, { dbName: 'mern-portafolio-eduardo' })
+  .then(() => {
+    console.log('Connected to DB successfully');
 
-
-//Connecting to mongo db using mongoose
-mongoose
-    .connect(process.env.mongo_url, {dbName : "mern-portafolio-eduardo"})
-    .then(() => {
-        console.log("Connected to DB successfully")
-
-        //Listening to request if db connection is successful
-        app.listen({port}, () => console.log(`Listening to port ${port}`))
-    })
-    .catch((err) => console.log(err))
+    // Listening to requests if db connection is successful
+    app.listen(port, () => console.log(`Listening on port ${port}`));
+  })
+  .catch((err) => console.log(err));
